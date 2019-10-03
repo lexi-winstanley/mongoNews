8e93cd21d860f90db5f4894a609557c93228ab98
lexi-winstanley <50344054+lexi-winstanley@users.noreply.github.com>
2019-10-02 20:55:17 -0700

    ready to be deployed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -70,11 +70,12 @@ module.exports = function (app) {
         }
     });
 
-    app.post('/api/notes', function (req, res) {
+    app.post('/api/notes', async function (req, res) {
         console.log(req.body);
-        db.Note.create(req.body)
+        const noteBody = {body: req.body.note}
+        await db.Note.create(noteBody)
         .then(function(dbNote) {
-          return db.Article.findOneAndUpdate({_id: req.params.id}, { $set: { note: dbNote._id } }, { new: true });
+          return db.Article.findOneAndUpdate({_id: req.body.id}, { $push: { notes: dbNote._id }}, { new: true, useFindAndModify: false });
         })
         .then(function(dbArticle) {
           res.json(dbArticle);
@@ -84,95 +85,15 @@ module.exports = function (app) {
         });
     });
 
-    app.put('/api/notes', function (req, res) {
-        const articleId = req.body.id;
-        let saveStatus;
+    app.delete('/api/notes', async function (req, res) {
         console.log(req.body);
-        console.log(req.params.id);
-        if (req.params.status === 'save') {
-            saveStatus = true;
-            db.Article.findOneAndUpdate({_id: articleId}, {saved: saveStatus})
-            .then(function (dbArticle) {
-                res.json(dbArticle);
-                console.log(dbArticle);
-            })
-            .catch(function (err) {
-                res.json(err);
-            });
-        } else if (req.params.status === 'remove') {
-            saveStatus = false;
-            db.Article.findOneAndUpdate({_id: articleId}, {saved: saveStatus})
-            .then(function (dbArticle) {
-                res.json(dbArticle);
-                console.log(dbArticle);
-            })
-            .catch(function (err) {
-                res.json(err);
-            });
-        }
+        await db.Note.deleteOne({_id : req.body.id})
+        .then(function(dbDeleted) {
+            console.log(dbDeleted);
+            res.json(dbDeleted);
+        })
+        .catch(function(err) {
+          res.json(err);
+        });
     });
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // app.get('/articles', function (req, res) {
-    //     db.Article.find({})
-    //     .then(function (dbArticles) {
-    //         res.json(dbArticles);
-    //     })
-    //     .catch(function (err) {
-    //         res.json(err);
-    //     });
-    // });
-
-
-
-
-
-//     app.get('/scrape', function (req, res) {
-//         // First, we grab the body of the html with axios
-//         axios.get('http://www.echojs.com/').then(function (response) {
-//             // Then, we load that into cheerio and save it to $ for a shorthand selector
-//             var $ = cheerio.load(response.data);
-
-//             // Now, we grab every h2 within an article tag, and do the following:
-//             $('article h2').each(function (i, element) {
-//                 // Save an empty result object
-//                 var result = {};
-
-//                 // Add the text and href of every link, and save them as properties of the result object
-//                 result.title = $(this)
-//                     .children('a')
-//                     .text();
-//                 result.link = $(this)
-//                     .children('a')
-//                     .attr('href');
-
-//                 // Create a new Article using the `result` object built from scraping
-//                 db.Article.create(result)
-//                     .then(function (dbArticle) {
-//                         // View the added result in the console
-//                         console.log(dbArticle);
-//                     })
-//                     .catch(function (err) {
-//                         // If an error occurred, log it
-//                         console.log(err);
-//                     });
-//             });
-
-//             // Send a message to the client
-//             res.send('Scrape Complete');
-//         });
-//     });
-// }
